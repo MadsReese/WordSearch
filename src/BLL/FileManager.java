d04c7ac3098cdd98498b940c4438f778c5348f92
MadsReese <mads.reese@gmail.com>
2013-02-19 09:53:32 +0100

    Initial addition of shizzle

---
+++ src/BLL/FileManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+/**
+ *
+ * @author Reese
+ */
+public class FileManager
+{
+    
+}
---
+++ src/DAL/FileAccess.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+/**
+ *
+ * @author Reese
+ */
+public class FileAccess
+{
+    
+}
