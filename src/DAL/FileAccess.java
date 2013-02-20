70d4dad303a7ba4ea691d5d260dfbbe3e8516680
MadsReese <mads.reese@gmail.com>
2013-02-20 09:17:29 +0100

    DAL finished so far

--- src/DAL/FileAccess.java
+++ src/DAL/FileAccess.java
@@ -5,7 +5,11 @@
 package DAL;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,25 +17,27 @@ import java.io.FileReader;
  */
 public class FileAccess
 {
-    
-    
-    public Array
-    BufferedReader br = new BufferedReader(new FileReader("brit-a-z.txt"));
-       
+
+    /**
+     * Gets the words from the dictionary file
+     * @return the words in a list
+     * @throws FileNotFoundException If the file cannot be found
+     * @throws IOException If the file cannot be read
+     */
+    public List<String> getWords() throws FileNotFoundException, IOException
+    {
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("brit-a-z.txt"));
+)
+        {
+            List<String> words = new ArrayList<>();
             String line = br.readLine();
 
             while (line != null)
             {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
+                words.add(line);
             }
-            String everything = sb.toString();
-    
-
-    
-    
-        finally {
-        br.close();
+            return words;
+        }
     }
 }
