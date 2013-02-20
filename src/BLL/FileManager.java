cb3f202b91948d39d8c4bd140daf36e5f9d7b6fa
MadsReese <mads.reese@gmail.com>
2013-02-20 10:31:44 +0100

    Adds beginsWith Creates a testing envi.

--- src/BLL/FileManager.java
+++ src/BLL/FileManager.java
@@ -7,6 +7,7 @@ package BLL;
 import DAL.FileAccess;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,12 +18,28 @@ public class FileManager
 {
     FileAccess fA = null;
     List<String> words = null;
-    
+    String[] wordArray = null;
+
     public FileManager() throws FileNotFoundException, IOException
     {
         fA = new FileAccess();
         words = fA.getAll();
     }
     
-    
+    public String[] beginsWith(String str)
+    {
+        wordArray = (String[]) words.toArray();
+        
+        //List<String> sortedWords = new ArrayList<>();
+        String[] sortedWords = null;
+        for(int i = 0; i < wordArray.length; i++)
+        {
+            if(wordArray[i].startsWith(str))
+            {
+                //sortedWords.add(str);
+                sortedWords[i] = str;
+            }
+        }
+        return sortedWords;
+    }
 }
---
+++ src/Testing/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testing;
+
+import BLL.FileManager;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Reese
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            FileManager fM = new FileManager();
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Derp");
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Derp");
+        }
+        
+        String[] word = fM.beginsWith("a");
+    }
+}
